bbce093986236bd1801bcd5d66b612e1f89e31fc
aleziz01 <alex.artene@gmail.com>
2025-04-19 15:08:30 +0200

    added a blue color pallette

--- src/gui.cpp
+++ src/gui.cpp
@@ -213,8 +213,21 @@ GLFWwindow* InitializeGUI(ImVec2 initDisplaySize) { // Generate the main window
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
     ImGuiStyle& style = ImGui::GetStyle();
+
+    // Window bg colors
     style.Colors[ImGuiCol_WindowBg] = ImVec4(3.0/255.0, 1.0/255.0, 44.0/255.0, 1);
-    style.Colors[ImGuiCol_Button] = ImVec4(0.00f, 0.08f, 0.06f, 1.00f);
+
+    // Button colors
+    style.Colors[ImGuiCol_Button] = ImVec4(0.1f, 0.2f, 0.5f, 1.00f);
+    style.Colors[ImGuiCol_ButtonHovered] = ImVec4(0.15f, 0.25f, 0.6f, 1.00f);
+    style.Colors[ImGuiCol_ButtonActive] = ImVec4(0.15f, 0.25f, 0.7f, 1.00f);
+
+    // Frame bg colors
+    style.Colors[ImGuiCol_FrameBg] = ImVec4(0.1f, 0.2f, 0.5f, 1.00f);
+    style.Colors[ImGuiCol_FrameBgHovered] = ImVec4(0.15f, 0.25f, 0.6f, 1.00f);
+    style.Colors[ImGuiCol_FrameBgActive] = ImVec4(0.15f, 0.25f, 0.7f, 1.00f);
+
+    // End of style setup
 
     //ImGui::StyleColorsLight();
 
